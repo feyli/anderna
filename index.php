d9134560be4f71c3563e9673288a46282cf0b5ea
IUT-Oriane <mejean.oriane@sfr.fr>
2025-09-18 09:37:19 +0200

    Page index très très basic (pour le moment)

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>DashMed</title>
+    <link rel="stylesheet" href=""> <!--inserer le nom du CSS-->
+</head>
+<body>
+<main>
+    <div class="container">
+        <h1>DashMed</h1>
+        <div class="buttons">
+            <a href="">Se connecter</a>
+            <a href="">S'inscrire</a>
+        </div>
+    </div>
+</main>
+</body>
+</html>
