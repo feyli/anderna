be7c6df38462da91db6e0d0a9a2f01fd962d7676
IUT-Oriane <mejean.oriane@sfr.fr>
2025-09-18 11:06:20 +0200

    Ajout d'un header & CSS

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <html lang="fr">
 <head>
     <title>DashMed</title>
-    <link rel="stylesheet" href=""> <!--inserer le nom du CSS-->
+    <link rel="stylesheet" href="_assets/includes/styles/styles.css"> <!--inserer le nom du CSS-->
 </head>
 <body>
+<?php include 'modules/blog/controllers/views/templates/header.php'; ?>
 <main>
     <div class="container">
         <h1>DashMed</h1>
         <div class="buttons">
-            <a href="">Se connecter</a>
-            <a href="">S'inscrire</a>
+            <a class="btn1" href="">Se connecter</a>
+            <a class="btn2" href="">S'inscrire</a>
         </div>
     </div>
 </main>
