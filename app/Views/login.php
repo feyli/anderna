55b88181386acb883288dde23299ce48b7a4c396
lilianValette <lilian.valette@etu.univ-amu.fr>
2025-10-05 10:24:51 +0100

    Add footer to login page and signup page

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/_assets/includes/styles/styles.css">
     <link rel="stylesheet" href="/_assets/includes/styles/styles-login.css">
+    <link rel="stylesheet" href="/_assets/includes/styles/styles-footer.css">
     <?php include __DIR__ . '/../../modules/controllers/views/templates/head.php'; ?>
     <title>Se connecter</title>
 </head>
@@ -69,6 +70,6 @@
             echo "Login failed!";
         }
     }
-    ?>
+    include __DIR__ . '/../../modules/controllers/views/templates/footer.php'; ?>
 </body>
 </html>
