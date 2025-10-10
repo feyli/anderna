2132f72beb86ed254285b81e918476b64b2c9864
Feyli <mathiscom64@gmail.com>
2025-10-10 11:25:46 +0200

    feat: add sitemap link to footer for improved navigation

--- modules/controllers/views/templates/footer.php
+++ modules/controllers/views/templates/footer.php
@@ -1,4 +1,5 @@
 <footer>
-    <a href="/legal-notice" >Mentions légales</a>
+    <a href="/legal-notice">Mentions légales</a>
+    <a href="/sitemap.xml">Plan du site</a>
     &ndash; Dashmed &copy; <?php echo date("Y"); ?> All rights reserved.
 </footer>
